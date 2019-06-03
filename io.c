cf3197f70a4d301e4fd92d7e462d1064f59760d5
Nick Korotysh <nick.korotysh@gmail.com>
2019-06-04 02:04:53 +0300

    move ufs_readlink implementation to links.c

--- io.c
+++ io.c
@@ -8,27 +8,6 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-int ufs_readlink(struct unityfs* fs, const char* path, char* buf, size_t sz)
-{
-  for (struct ufs_disk* disk = fs->all_disks; disk != fs->all_disks + fs->disks_count; ++disk) {
-    char* real_path = get_real_path(disk, path);
-
-    errno = 0;
-    struct stat st_buf;
-
-    if (lstat(real_path, &st_buf) == 0 && S_ISLNK(st_buf.st_mode)) {
-      ssize_t lsz = readlink(real_path, buf, sz);
-      if (lsz >= 0)
-        buf[(size_t)lsz < sz - 1 ? (size_t)lsz : sz - 1] = '\0';
-      free(real_path);
-      break;
-    }
-
-    free(real_path);
-  }
-  return -errno;
-}
-
 int ufs_mkdir(struct unityfs* fs, const char* path, mode_t mode)
 {
   char* real_path = new_real_path(fs, path);
